c1756ef4d0947f7b48bb3fac27351548bf559b15
adriata-edu-ocean <eduardolimaalencar751@gmail.com>
2026-05-15 05:27:08 +0000

    Exercício 1 de laço while concluído

---
+++ teste.c
@@ -0,0 +1,16 @@
+//Atividade 1
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int Numero;
+	printf("Digite números inteiro ou 0 para sair: \n");
+	scanf("%i", &Numero);
+	while(Numero != 0)
+	{
+	scanf("%i", &Numero);
+	}
+	printf("Fim do Laço");	
+}
